add3483934a68bb86a635c5ccb3a35bc867697a7
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-12 20:21:19 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,32 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
-
   return (
+    <Link
+      to={`/movie/${movie.imdbID}`}
+      className="movie-link"
+    >
+      <div className="card">
 
-    <div className="card">
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/300x450"
+          }
+          alt={movie.Title}
+        />
 
-      <img
-        src={
-          movie.Poster !== "N/A"
-            ? movie.Poster
-            : "https://via.placeholder.com/300x450"
-        }
-        alt={movie.Title}
-      />
+        <div className="info">
+          <h2>{movie.Title}</h2>
 
-      <div className="info">
+          <p>📅 {movie.Year}</p>
 
-        <h2>{movie.Title}</h2>
+          <p>🎬 {movie.Type}</p>
 
-        <p>{movie.Year}</p>
-
-        <p>{movie.Type}</p>
+          <button className="details-btn">
+            View Details
+          </button>
+        </div>
 
       </div>
-
-    </div>
-
+    </Link>
   );
 }
 
