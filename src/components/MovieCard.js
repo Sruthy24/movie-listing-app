6d264622803f704167893a4410443d3f0edc83df
Sruthy24 <sruthysmnglm@gmail.com>
2026-07-12 21:03:40 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MovieCard({ movie }) {
-
-  const navigate = useNavigate();
-
   return (
-    <div 
-      className="card"
-      onClick={() => navigate(`/movie/${movie.imdbID}`)}
+    <Link 
+      to={`/movie/${movie.imdbID}`} 
+      className="movie-link"
     >
+      <div className="card">
 
-      <img
-        src={
-          movie.Poster !== "N/A"
-            ? movie.Poster
-            : "https://via.placeholder.com/300x450"
-        }
-        alt={movie.Title}
-      />
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/300x450"
+          }
+          alt={movie.Title}
+        />
 
-      <h2>{movie.Title}</h2>
-      <p>{movie.Year}</p>
+        <h2>{movie.Title}</h2>
 
-    </div>
+        <p>{movie.Year}</p>
+
+      </div>
+    </Link>
   );
 }
 
